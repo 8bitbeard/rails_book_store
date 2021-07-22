f30abb4297a8aebb7b915e4c99aa93cd38eb4bd1
Wilton Souza <willsouzafilho@gmail.com>
2021-07-22 07:53:30 -0300

    Adding unit test structure and first test

--- book_store/app/controllers/api/v1/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
-
-  def not_destroyed(e)
-    obj = { errors: e.record.errors }
-    render json: obj, status: :unprocessable_entity
-  end
-end
--- book_store/app/controllers/api/v1/books_controller.rb
+++ book_store/app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class BooksController < ApplicationController
-
       def index
         render json: Book.all
       end
