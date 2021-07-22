2a525913f9d9308e4f891b936f8f3494774bd825
Wilton Souza <willsouzafilho@gmail.com>
2021-07-22 07:24:04 -0300

    Adding the namespacing and versioning the API

---
+++ book_store/app/controllers/api/v1/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  def not_destroyed(e)
+    obj = { errors: e.record.errors }
+    render json: obj, status: :unprocessable_entity
+  end
+end
--- book_store/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::API
-  recue_from ActiveRecord::RecordNotDestroyed, with :not_destroyed
-
-  private
-
-  def not_destroyed(e)
-    render json: {errors: e.record.errors}, status: :unprocessable_entity
-  end
-end
